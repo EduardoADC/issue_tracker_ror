958d0e7b9641b99902e0e4acfb5fcc493739874f
Eduardo Diosdado <eduardodiosdado00@gmail.com>
2023-11-17 16:29:49 -0600

    Route refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :projects
+  # API routes are in /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
